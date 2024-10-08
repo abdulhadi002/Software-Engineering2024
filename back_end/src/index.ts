b462f33ab8a249ccf4cc88d70677a90e6863cdab
D3S002 <abdulhma@hiof.no>
2024-10-08 20:30:36 +0200

    Update index.ts

--- back_end/src/index.ts
+++ back_end/src/index.ts
@@ -17,25 +17,25 @@ app.get("/json", async (c) => {
 
 app.post("/json", async (c) => {
   try {
-    const newProject = await c.req.json();
+    const newDevice = await c.req.json();
     const post_device = await readFile("./backend/src/data.json", "utf-8");
     const devices = JSON.parse(post_device).devices;
 
-    newProject.id = devices.length ? devices[devices.length - 1].id + 1 : 1;
-    devices.push(newProject);
+    newDevice.id = devices.length ? devices[devices.length - 1].id + 1 : 1;
+    devices.push(newDevice);
 
     await writeFile("./backend/src/data.json", JSON.stringify({ device: devices }, null, 2));
 
-    return c.json(newProject, 200);
+    return c.json(newDevice, 200);
   } catch (error) {
     return c.text("Failed to save device", 500);
   }
 });
 
-const port = 3000
+const port = 6969
 console.log(`Server is running on port ${port}`)
 
 serve({
   fetch: app.fetch,
   port
-})
+});
