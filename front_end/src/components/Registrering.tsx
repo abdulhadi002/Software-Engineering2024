e550a0a684bf1cd51cf4c2e122ba0245b39b16ef
D3S002 <abdulhma@hiof.no>
2024-10-08 19:09:08 +0200

    json fil
    jeg har lagt en json fil med hvordan data.json vår skal se ut.

--- front_end/src/components/Registrering.tsx
+++ front_end/src/components/Registrering.tsx
@@ -1,7 +1,43 @@
+import * as React from "react";
+
+
+
 type RegistreringProps = {
   onRegister: (username: string, password: string) => void;
 };
 
 export default function Registrering(props: RegistreringProps) {
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    props.onRegister(username, password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
+      <button type="submit">Register</button>
+    </form>
+  );
 }
-//github test
