98d5f1d6707dde389ace3af8fb8aa0a743662494
oboy047 <obaidataha16@gmail.com>
2024-10-23 15:17:35 +0200

    Layout
    Update

---
+++ front_end/src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Footer.css'; // Link to Footer.css
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="footer">
+      <p>Footer content goes here</p>
+    </footer>
+  );
+};
+
+export default Footer;
